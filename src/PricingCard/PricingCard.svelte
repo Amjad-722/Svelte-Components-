dffc8d3715cacb65750995011f573b0fde9a6989
Amjad-722 <aliamjad722ee@gmail.com>
2024-10-11 22:20:28 +0500

    updated princing card

--- src/PricingCard/PricingCard.svelte
+++ src/PricingCard/PricingCard.svelte
@@ -40,12 +40,17 @@
     <div class="grid md:grid-cols-3 sm:grid-cols-1 space-x-6">
         {#each pricingPlans as plan}
             <div class={`pricing-card flex flex-col border-2 hover:-translate-y-4 hover:shadow-7xl hover:shadow-stone-800 text-start px-16 items-start rounded-3xl border-gray-500 shadow-xl shadow-gray-700 ${plan.bgColor}`} style="background-image: url({plan.bgimage}); background-size: cover; background-repeat: no-repeat;">
-                <span class={`font-semibold pt-8 text-start w-full ${plan.bgText}`}>{plan.plan}</span>
+                <span class={`font-semibold pt-8 text-start py-1 w-full ${plan.bgText}`}>{plan.plan}</span>
                 <span class={`text-start w-full ${plan.bgText}`}>{plan.description}</span>
                 <p class={`text-5xl font-bold py-5 text-start w-full ${plan.bgText}`}>{plan.price}</p>
-                <ul class="flex flex-col my-14 text-start w-full">
+                <ul class="flex   flex-col my-14 text-start w-full">
                     {#each plan.features as feature}
-                        <li class={`font-semibold ${plan.bgText}`}>{feature}</li>
+                    <div class="flex py-2 flex-row"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                      </svg>
+                      <li class={`font-semibold ${plan.bgText}`}>{feature}</li>
+                      </div>
+                        
                     {/each}
                 </ul>
                 <button class="border-2 py-2 px-10 my-10 rounded-full bg-gradient-to-r hover:to-cyan-500 shadow-md shadow-slate-600 to-gray-400 from-white">
