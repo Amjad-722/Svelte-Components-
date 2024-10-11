87f4d4b4ac8769f1f4c820a85b6f3005cee92f5f
Amjad-722 <aliamjad722ee@gmail.com>
2024-10-11 22:29:44 +0500

    updated princing card

--- src/PricingCard/PricingCard.svelte
+++ src/PricingCard/PricingCard.svelte
@@ -45,7 +45,7 @@
                 <p class={`text-5xl font-bold py-5 text-start w-full ${plan.bgText}`}>{plan.price}</p>
                 <ul class="flex   flex-col my-14 text-start w-full">
                     {#each plan.features as feature}
-                    <div class="flex py-2 text-fill flex-row"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <div class="flex py-2 gap-2 text-fill flex-row"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                       </svg>
                       <li class={`font-semibold ${plan.bgText}`}>{feature}</li>
