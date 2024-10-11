c7c30140b185fc30905bc051e11f7dd713d1b7bd
Amjad-722 <aliamjad722ee@gmail.com>
2024-10-11 19:16:01 +0500

    update

--- src/PricingCard/PricingCard.svelte
+++ src/PricingCard/PricingCard.svelte
@@ -4,21 +4,20 @@
 
 </script>
 
-<main>
+<main class="h-screen">
 <div class="">
     <div class="flex max-w-xs mx-auto flex-col border-2  text-start px-6  items-center rounded-3xl border-gray-500 shadow-xl shadow-gray-700"> 
         <span class="font-semibold  pt-8">free</span>
         <span> freelancer</span>
-        <p class="text-5xl font-bold py-5">$20</p>
-        <ul class="flex flex-col mt-16">
+        <p class="text-5xl font-bold py-5">$0</p>
+        <ul class="flex flex-col my-14">
             <li class="font-semibold text-gray-500">1 user</li>
             <li class="font-semibold text-gray-500"> 10 projects</li>
             <li class="font-semibold text-gray-500">Rosters files</li>
         </ul>
-        <button class="border-2  mx-10  py-4  rounded-full bg-gradient-to-r   hover:to-cyan-500 px-6  shadow-md shadow-slate-600 to-gray-400   from-white">
+<buttons class="border-2  py-2 p-10 my-10 rounded-full bg-gradient-to-r   hover:to-cyan-500 px-4  shadow-md shadow-slate-600 to-gray-400   from-white">get started</buttons>
     </div>
-    <div></div>
-    <div></div>
+    
 </div>
 
 </main>
