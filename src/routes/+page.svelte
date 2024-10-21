c4413087b58b7237e002848038ec77f2215d4094
Amjad-722 <aliamjad722ee@gmail.com>
2024-10-21 18:55:39 +0500

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,16 +4,17 @@
   import Navabr from "../Navbar/Navabr.svelte";
   import Nvabr from "../Navbar/Navabr.svelte";
 import Notificationes from "../Notificationes/Notificationes.svelte";
+  import HoveredCards from "../PricingCard/hovedCard/hoveredCards.svelte";
   import PricingCard from "../PricingCard/PricingCard.svelte";
 
   
   </script>
 
   <main>
-    <div>
+    <!-- <div>
       <h1 class=" mt-8 text-3xl font-bold flex justify-center text-gray-500 items-center">BreadCrumd</h1>
       <Card/>
-    </div>
+    </div> -->
     <hr class="border-1 border-gray-400 ">
     <div class="p-5 mt-8">
       <h1 class="text-3xl font-bold flex justify-center text-gray-500 items-center">Navbar</h1>
@@ -35,6 +36,12 @@ import Notificationes from "../Notificationes/Notificationes.svelte";
       <h1 class=" text-3xl mt-40 font-bold flex justify-center text-gray-500 items-center ">Pricing Card</h1>
 <PricingCard/>
     </div>
+    <div class="py-5">
+
+      <h1 class=" text-3xl mt-40 font-bold flex justify-center text-gray-500 items-center ">Hover Cards</h1>
+<HoveredCards/>
+    </div>
+
 
    
   </main>
